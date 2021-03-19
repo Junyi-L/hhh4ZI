ec7e8b04f8fb1df421a405ddd4fa137437df65c8
JL <junyi.lu@fau.de>
2021-03-19 19:20:43 +0100

    refit hhh4

---
+++ R/hhh4ZI_hhh4.R
@@ -0,0 +1,17 @@
+#' @title Refit a HHH4 model to a HHH4ZI model
+#' @export
+hhh4ZI.hhh4 <- function(object, # a HHH4 object
+                        zi = list(f = ~-1,
+                                  lag = 1,
+                                  lag.unitSpecific = FALSE
+                        ) # control list for zero model part
+                        ){
+  stopifnot(is.list(zi))
+  control <- object$control
+  control$zi <- zi
+  # use fitted coefficients in hhh4 object as start value
+  control$start <- surveillance:::hhh4coef2start(object)
+  ## call main function
+  hhh4ZI.sts(object$stsObj, control)
+
+}
