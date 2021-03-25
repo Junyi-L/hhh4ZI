a3528904f7ed91b16eb5141538de07f971d86fe9
JL <junyi.lu@fau.de>
2021-03-25 11:10:31 +0100

    can also update control list in other parts

--- R/hhh4ZI_hhh4.R
+++ R/hhh4ZI_hhh4.R
@@ -1,16 +1,26 @@
 #' @title Refit a HHH4 model to a HHH4ZI model
 #' @export
 hhh4ZI.hhh4 <- function(object, # a HHH4 object
-                        zi = list(f = ~-1,
+                        control = list(f = ~-1,
                                   lag = 1,
                                   lag.unitSpecific = FALSE
-                        ) # control list for zero model part
+                        ),... # control list for zero model part
                         ){
-  stopifnot(is.list(zi))
-  control <- object$control
-  control$zi <- zi
-  # use fitted coefficients in hhh4 object as start value
-  control$start <- surveillance:::hhh4coef2start(object)
+  stopifnot(is.list(control))
+  # all_control <- object$control
+  # all_control$zi <- control
+
+  ## use update.hhh4() to merge control arguments
+  args <- if ("f" %in% names(control))  # zi control list
+    list(zi = control, ...)
+  else  # hhh4 control list with zi list included
+    c(control, list(...))
+  args$object <- quote(object)
+  args$evaluate <- FALSE
+  control <- do.call(update.hhh4ZI, args)
+
+  # use fitted coefficients in hhh4 object as start value, dimension is different
+  # when having random effect
   control$start$sd.corr <- NULL
   ## call main function
   hhh4ZI.sts(object$stsObj, control)
