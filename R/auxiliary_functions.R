2b0084ece746b5b15e90a7868d9c7b1aa09bc94c
JL <junyi.lu@fau.de>
2021-03-28 19:29:14 +0200

    data documentation

--- R/auxiliary_functions.R
+++ R/auxiliary_functions.R
@@ -9,6 +9,35 @@
 #' from week 1 in 2005.The \code{population} slot contains the population fractions
 #' of each state at 31.12.2006, obtained from the Federal Statistical Office of Germany.
 #' @source SurvStat@RKI 2.0 (https://survstat.rki.de),
-#' Robert Koch Institute; Queried on 20 February 2021.
+#' Robert Koch Institute; Queried on 26 March 2021.
+#' @keywords datasets
+NULL
+
+#' @title MMR coverage levels in the 16 states of Germany
+#' @description Coverage levels at school entry for the first and second dose
+#' of the combined measles-mumps-rubella (MMR) vaccine in 2005 to 2018,
+#' estimated from children presenting vaccination documents at school entry examinations.
+#'
+#' @name Coverage
+#' @docType data
+#' @usage data(Coverage)
+#' @format Four \code{data.frame},
+#' \itemize{
+#' \item{TotalKids} Number of children examined.
+#' \item{VacPass} Number of children who presented vaccination documents.
+#' \item{Dosis1} Percentage of children with vaccination documents,
+#' who received at least 1 dose of MMR vaccine.
+#' \item{Dosis2} Percentage of children with vaccination documents,
+#'  who received at least 2 doses of MMR vaccine.
+#' }
+#'
+#' Coverage levels were derived from vaccination documents presented at medical examinations,
+#' which are conducted by local health authorities at school entry each year.
+#' Records include information about the receipt of 1st and 2nd doses of MMR,
+#' from year 2005 to 2018. Note that information from children who did not
+#' present a vaccination document on the day of the medical examination,
+#' is not included in the estimated coverage.
+#' @source https://www.gbe-bund.de,
+#' Gesundheitsbericherstattung des Bundes; Queried on 23 February 2021.
 #' @keywords datasets
 NULL
--- develop/HHH4ZI_fit.R
+++ develop/HHH4ZI_fit.R
@@ -1,4 +1,4 @@
-library(hhh4ZI)
+# library(hhh4ZI)
 data("measlesDE", package = "hhh4ZI")
 measlesDE <- aggregate(measlesDE, by = "time", nfreq = 26)
 
@@ -7,15 +7,16 @@ neW <- neighbourhood(measlesDE)
 adjmat <- poly2adjmat(measlesDE@map)
 neW1 <- adjmat/colSums(adjmat)
 
-f.end <- addSeason2formula(f = ~-1 + fe(1, unitSpecific = TRUE),
+f.1 <- addSeason2formula(f = ~-1 + fe(1, unitSpecific = TRUE),
                            S = 1, period = 26)
-# f.gamma <- addSeason2formula(f = ~-1 + fe(1, unitSpecific = TRUE),
-#                              S = 1, period = 26)
+f.2 <- addSeason2formula(f = ~-1 + fe(1, unitSpecific = FALSE),
+                             S = 1, period = 26)
+f.3 <- addSeason2formula(f = ~-1 + fe(1, unitSpecific = FALSE))
 # f.ar <- ~ -1 + fe(1, unitSpecific = TRUE)
 # f.ne <- ~-1 + fe(1, unitSpecific = TRUE)
 fitZi <- hhh4ZI(stsObj,
                 control = list(
-                  ar = list(f = ~1,        # a formula "exp(x'lamba)*y_t-lag" (ToDo: matrix)
+                  ar = list(f = f.3,        # a formula "exp(x'lamba)*y_t-lag" (ToDo: matrix)
                             offset = 1,      # multiplicative offset
                             lag = 1),        # autoregression on y_i,t-lag
                   ne = list(f = ~1,        # a formula "exp(x'phi) * sum_j w_ji * y_j,t-lag"
@@ -24,9 +25,9 @@ fitZi <- hhh4ZI(stsObj,
                             weights = neW1,  # weights w_ji
                             scale = NULL,    # such that w_ji = scale * weights
                             normalize = TRUE), # w_ji -> w_ji / rowSums(w_ji), after scaling
-                  end = list(f = f.end,        # a formula "exp(x'nu) * n_it"
+                  end = list(f = f.1,        # a formula "exp(x'nu) * n_it"
                              offset = 1),    # optional multiplicative offset e_it
-                  zi = list(f = ~1,
+                  zi = list(f = f.1,
                             lag = 1        # can be a scalar or vector
                   ),
                   family = c("NegBin1"), # or a factor of length nUnit for Negbin
@@ -43,14 +44,14 @@ fitZi <- hhh4ZI(stsObj,
                 )
 )
 summary(fitZi)
-fitZi2 <- hhh4ZI:::update.hhh4ZI(fitZi,subset.upper=370)
 fitZi2 <- update(fitZi,subset.upper=370)
 summary(fitZi2)
-pred <- oneStepAhead(fitZi, 350, type = "rolling", which.start = "current")
+pred <- oneStepAhead(fitZi, 318, type = "rolling", which.start = "current")
 x <- hhh4ZI:::quantile.oneStepAhead_hhh4ZI(pred)
 hhh4ZI:::plot.oneStepAhead_hhh4ZI(pred, unit = 11)
 scores(pred)
 scores(fitZi2)
+pit(pred, units = 16)
 
 fitH <- hhh4(stsObj,
              control = list(
@@ -79,6 +80,45 @@ fitH <- hhh4(stsObj,
              ), check.analyticals = FALSE
 )
 summary(fitH)
-fitH_ZI <- hhh4ZI(fitH, zi = list(f =  ~-1 + ri(type = "iid", corr = "all"),
-                       lag = 1        # can be a scalar or vector
+fitH_ZI <- hhh4ZI(fitH, control = list(f =  ~-1 + ri(type = "iid", corr = "all"),
+                                       lag = 1        # can be a scalar or vector
 ))
+
+fitH_ZI <- hhh4ZI(fitH,
+                  control = list(
+                    ar = list(f = ~1,        # a formula "exp(x'lamba)*y_t-lag" (ToDo: matrix)
+                              offset = 1,      # multiplicative offset
+                              lag = 1),        # autoregression on y_i,t-lag
+                    ne = list(f = ~-1 + ri(type = "iid", corr = "all"),        # a formula "exp(x'phi) * sum_j w_ji * y_j,t-lag"
+                              offset = 1,      # multiplicative offset
+                              lag = 1,         # regression on y_j,t-lag
+                              weights = neW1,  # weights w_ji
+                              scale = NULL,    # such that w_ji = scale * weights
+                              normalize = TRUE), # w_ji -> w_ji / rowSums(w_ji), after scaling
+                    end = list(f = ~-1 + ri(type = "iid", corr = "all"),        # a formula "exp(x'nu) * n_it"
+                               offset = 1),    # optional multiplicative offset e_it
+                    zi = list(f =  ~-1 + ri(type = "iid", corr = "all"),
+                           lag = 1),       # can be a scalar or vector
+                    family = c("NegBin1"), # or a factor of length nUnit for Negbin
+                    #subset = 2:nrow(stsObj),   # epidemic components require Y_{t-lag}
+                    optimizer = list(stop = list(tol = 1e-5, niter = 200), # control arguments
+                                     regression = list(method = "nlminb"), # for optimization
+                                     variance = list(method = "Nelder-Mead")),  # <- or "Nelder-Mead"
+                    verbose = TRUE,           # level of reporting during optimization
+                    start = list(fixed = NULL, # list of start values, replacing initial
+                                 random = NULL,  # values from fe() and ri() in 'f'ormulae
+                                 sd.corr = NULL),
+                    data = list(t = stsObj@epoch - min(stsObj@epoch)), # named list of covariates
+                    keep.terms = TRUE  # whether to keep interpretControl(control, stsObj)
+                  )
+)
+surveillance:::plot(fitH)
+#plotHHH4ZI_fitted(fitH_ZI, decompose = TRUE)
+
+
+plotHHH4ZI_season(fitZi)
+plotHHH4ZI_neweights(fitH_ZI)
+plot(fitH_ZI, type = "ri", component = "zi.ri(iid)")
+plot(fitH_ZI, type = "neweights")
+plot(fitH_ZI, type = "fitted", unit = 3)
+plot(fitH_ZI, type = "map")
