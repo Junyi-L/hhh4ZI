0698d947668df715491d7aa9c26c8efb9cf77ead
Sebastian Meyer <seb.meyer@fau.de>
2021-09-23 17:33:25 +0200

    add population as map data

--- develop/load_measles_data.R
+++ develop/load_measles_data.R
@@ -1,22 +1,23 @@
+#!/usr/bin/env Rscript
+## this script generates data/measles.rda and data/Coverage.rda
+
 library("surveillance")
-library("xlsx")
+library("openxlsx")
 library("here")
 
-#measles <- read.csv(file = "measles.csv", header = TRUE)
-
-measles <- read.xlsx(file = here("./develop/data.xlsx"),sheetIndex = 1, header = TRUE)
+measles <- read.xlsx(here("develop", "data.xlsx"))
 measles$Unknown <- NULL
 measles[is.na(measles)] <- 0
 names(measles)[1] <- "date"
 measles$year <- as.numeric(substr(measles$date, 1, 4))
-measles$week <- as.numeric(substr(measles$date, 7, 8))
+#measles$week <- as.numeric(substr(measles$date, 7, 8))
 
 start_year <- which(measles$year == 2005)[1]
 # year 2005 - 2018
 measles <- measles[start_year : (start_year + 14*52 - 1),]
 measles[ ,c("date", "year")] <- NULL
 
-load(system.file("shapes/districtsD.RData", package = "surveillance"))
+load(system.file("shapes", "districtsD.RData", package = "surveillance"))
 #plot(districtsD)
 
 statesD <- rgeos::gUnaryUnion(districtsD,
@@ -25,7 +26,7 @@ statesD <- rgeos::gUnaryUnion(districtsD,
 # plot(statesD)
 # text(coordinates(statesD), labels = row.names(statesD))
 
-row.names(statesD) <- namesD <- c(
+row.names(statesD) <- namesDE <- c(
   "Schleswig-Holstein",
   "Hamburg",
   "Lower Saxony",
@@ -44,60 +45,56 @@ row.names(statesD) <- namesD <- c(
   "Thuringia"
 )
 
+## add population as map data
+namesDE <- sort(namesDE)
+Population <- read.xlsx(here("develop", "population.xlsx"),
+                        sheet = 3, startRow = 2, rowNames = TRUE)
+colnames(Population) <- namesDE[order(colnames(Population))]
+Population <- Population[, row.names(statesD)]
+rownames(Population) <- paste0("POP", rownames(Population))
+statesD <- SpatialPolygonsDataFrame(statesD, as.data.frame(t(Population)))
+
+## compute matrix of adjacency orders
 adjmat <- poly2adjmat(statesD)
 adjord <- nbOrder(adjmat, maxlag = Inf)
 
-namesDE <- colnames(adjord)
-namesDE <- namesDE[order(namesDE)]
+## sync columns
 colnames(measles) <- namesDE[order(colnames(measles))]
 measles <- measles[, colnames(adjord)] # reorder
 
-measles <- sts(observed = measles, start = c(2005, 1),
-                 neighbourhood = adjord)
-# aggregate data when using
+## create "sts" object
+measles <- sts(observed = measles, start = c(2005, 1), frequency = 52,
+               map = statesD, neighbourhood = adjord)
+# aggregate to bi-weekly counts
 measles <- aggregate(measles, by = "time", nfreq = 26)
 
-measles@map <- statesD
-#measles@neighbourhood <- adjord
-
-Population <- read.xlsx(here("./develop/population.xlsx"),
-                        sheetIndex = 3, colIndex = 2:17, startRow = 2)
-colnames(Population) <- namesDE[order(colnames(Population))]
-Population <- Population[, colnames(adjord)]
-rownames(Population) <- 2005:2018
+## add population fractions to sts object
 Population <- Population[rep(1 : nrow(Population), each = 26),]
 Population <- Population/rowSums(Population)
-measles@populationFrac <- as.matrix(Population)
+measles@populationFrac <- as.matrix(Population, rownames = FALSE)
 
-save(measles, file = here("./data/measles.rda"))
+## done with measles data
+save(measles, file = here("data", "measles.rda"))
 
 
-TotalKids <- read.xlsx(here("./develop/coverage.xlsx"),
-                sheetIndex = 1, colIndex = 2:17, startRow = 2)
-colnames(TotalKids) <- namesDE[order(colnames(TotalKids))]
-TotalKids <- TotalKids[, colnames(adjord)]
-rownames(TotalKids) <- 2005:2018
+### load vaccination coverage
 
-VacPass <- read.xlsx(here("./develop/coverage.xlsx"),
-                       sheetIndex = 2, colIndex = 2:17, startRow = 2)
-colnames(VacPass) <- namesDE[order(colnames(VacPass))]
-VacPass <- VacPass[, colnames(adjord)]
-rownames(VacPass) <- 2005:2018
+TotalKids <- read.xlsx(here("develop", "coverage.xlsx"),
+                       sheet = 1, startRow = 2, rowNames = TRUE, sep.names = " ")
+TotalKids <- TotalKids[, colnames(measles)]
 
-Dosis1 <- read.xlsx(here("./develop/coverage.xlsx"),
-                     sheetIndex = 3, colIndex = 2:17, startRow = 2)
-colnames(Dosis1) <- namesDE[order(colnames(Dosis1))]
-Dosis1 <- Dosis1[, colnames(adjord)]
-rownames(Dosis1) <- 2005:2018
+VacPass <- read.xlsx(here("develop", "coverage.xlsx"),
+                     sheet = 2, startRow = 2, rowNames = TRUE, sep.names = " ")
+VacPass <- VacPass[, colnames(measles)]
 
-Dosis2 <- read.xlsx(here("./develop/coverage.xlsx"),
-                     sheetIndex = 4, colIndex = 2:17, startRow = 2)
-colnames(Dosis2) <- namesDE[order(colnames(Dosis2))]
-Dosis2 <- Dosis2[, colnames(adjord)]
-rownames(Dosis2) <- 2005:2018
+Dosis1 <- read.xlsx(here("develop", "coverage.xlsx"),
+                    sheet = 3, startRow = 2, rowNames = TRUE, sep.names = " ")
+Dosis1 <- Dosis1[, colnames(measles)]
+
+Dosis2 <- read.xlsx(here("develop", "coverage.xlsx"),
+                    sheet = 4, startRow = 2, rowNames = TRUE, sep.names = " ")
+Dosis2 <- Dosis2[, colnames(measles)]
 
 
 save(list = c("TotalKids", "VacPass",
               "Dosis1", "Dosis2"), file = here("./data/Coverage.rda"))
-
-
